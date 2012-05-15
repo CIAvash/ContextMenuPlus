a2ff258a4ed6c1b6a8ec4f2ebfdd0b0346540614
Siavash Askari Nasr <siavash.askari.nasr@gmail.com>
2012-05-15 13:15:55 +0430

    Added new hotkey for "Paste & submit".

---
+++ data/js/paste-and-submit-hotkey.js
@@ -0,0 +1,16 @@
+function getAncestorByTagName(node, tag){
+    for(tag = tag.toUpperCase(); node && node.tagName !== tag; node = node.parentNode){}
+    return node;
+}
+if(document.activeElement && ( (document.activeElement.tagName == "INPUT" && document.activeElement.type == "text") || document.activeElement.tagName == "TEXTAREA" ) ) {
+    var node = document.activeElement;
+    var form = getAncestorByTagName(node, "form");
+    if(form) self.postMessage();
+}
+self.on("message", function(data) {
+    var strPos = node.selectionStart;
+        node.value = node.value.substring(0, strPos) + data + node.value.substring(node.selectionEnd, node.value.length);
+        node.selectionStart = node.selectionEnd = strPos + data.length;
+        node.focus();
+        form.submit();
+});
