916296868bf02003a163f02829ecb5fd13a35f18
Siavash Askari Nasr <siavash.askari.nasr@gmail.com>
2012-08-25 16:49:53 +0430

    Added "password" type to Paste & Submit

--- data/js/paste-and-submit-hotkey.js
+++ data/js/paste-and-submit-hotkey.js
@@ -2,7 +2,7 @@ function getAncestorByTagName(node, tag){
     for(tag = tag.toUpperCase(); node && node.tagName !== tag; node = node.parentNode){}
     return node;
 }
-if(document.activeElement && ( (document.activeElement.tagName.toLowerCase() == "input" && document.activeElement.type == "text") || document.activeElement.tagName.toLowerCase() == "textarea" ) ) {
+if(document.activeElement && ( (document.activeElement.tagName.toLowerCase() == "input" && (document.activeElement.type == "text" || document.activeElement.type == "password")) || document.activeElement.tagName.toLowerCase() == "textarea" ) ) {
     var node = document.activeElement;
     var form = getAncestorByTagName(node, "form");
     if(form) self.postMessage();
